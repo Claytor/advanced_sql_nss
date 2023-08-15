cc22466f7877cdb4c0611ff4f7fc7c799140fe53
Claytor <tim@claytor.me>
2023-08-14 20:57:54 -0500

    Decided to answer all questions in one query with 4 CTEs.

--- Book 2 - Carnival Reports/ch7_common_table_expressions.sql
+++ Book 2 - Carnival Reports/ch7_common_table_expressions.sql
@@ -1,125 +1,81 @@
-/*Insert some oil change records into the database.*/
--- INSERT INTO
--- 	oilchangelogs
--- 	(date_occured, vehicle_id)
--- VALUES
---     ('2020-01-09', 1),
---     ('2021-10-30', 2),
---     ('2019-02-20', 3),
---     ('2022-03-17', 4);
-
-/*Create a CTE to get cars that require an oil change service.*/
---looks like 15 people need an oil change
-WITH vehicles_needing_service AS
-(
-	SELECT
-		v.vehicle_id,
-        v.year_of_car,
-        v.miles_count,
-        TO_CHAR(o.date_occured, 'YYYY-MM-DD') date_of_last_change
-	FROM vehicles v
-	JOIN oilchangelogs o
-		ON v.vehicle_id = o.vehicle_id
-	WHERE o.date_occured < '2022-01-01'
-)
-SELECT	
-	vs.vehicle_id,
-	vs.miles_count,
-	s.purchase_date,
-	e.first_name ||' '|| e.last_name AS seller,
-	c.first_name ||' '|| c.last_name AS purchaser,
-	c.email
-FROM vehicles_needing_service vs
-JOIN sales s
-	ON s.vehicle_id = vs.vehicle_id
-JOIN employees e
-	ON s.employee_id = e.employee_id
-JOIN customers c
-	ON s.customer_id = c.customer_id
-ORDER BY
-	vs.vehicle_id,
-	s.purchase_date DESC;
-	
-/*Since multiple people can purchase the same car over time, the service manager only wants the last purchaser. You can break this part of the SQL out into another CTE. You can use multiple CTEs as part of the query. You separate them with a comma.*/
-WITH vehicles_needing_service AS
-(
-	SELECT
-		v.vehicle_id,
-        v.year_of_car,
-        v.miles_count,
-        TO_CHAR(o.date_occured, 'YYYY-MM-DD') date_of_last_change
-	FROM vehicles v
-	JOIN oilchangelogs o
-		ON v.vehicle_id = o.vehicle_id
-	WHERE o.date_occured < '2022-01-01'
-),
-last_purchase AS
-(
-	SELECT
-		s.vehicle_id,
-		MAX(s.purchase_date) AS purchased
-	FROM sales s
-	GROUP BY s.vehicle_id
-)
-SELECT
-	vc.vehicle_id,
-	vs.miles_count,
-	lp.purshaces,
-	e.first_name ||' '|| e.last_name seller,
-	c.first_name ||' '|| c.purchaser,
-	e.email
-FROM vehicles_needing_service vs
-JOIN last_purchase
-	ON lp.vehicle_id = vs.vehicle_id
-JOIN sales s
-	ON lp.vehicle_id = s.vehicle_id
-	AND lp.purchased = s.purchase_date
-JOIN employees e
-	ON s.employee_id = e.employee_id
-JOIN customers c
-	ON s.customer_id = c.customer_id
-	
 --Top 5 Dealerships--
-/*For the top 5 dealerships, which employees made the most sales? I am going to choose to define "best" as highest arithmetic mean of employee to overall sales revenue*/
+-- I am going to try to answer all quesitons in ONE table . . .
+--For the top 5 dealerships: 
 
-/*My strategy is to use the arithmetic mean for all sales types per each employee per dealership to make fair compairisons between dealerships with unequal staffing.  I will use this metric to differentiate the top 5 performing dealerships. Note that using a weighed average may be more appropriate for finding profit per employee.  However, the margins per sales type are not provided*/
+-- which employees made the most sales (I will intrepret as sales which were not returned), 
+-- which vehicle models were the most popular in sales and 
+-- were there more sales or leases?
+
+/*My strategy is to use the arithmetic mean for all sales types per each employee per dealership to make fair compairisons between dealerships with unequal staffing.  I will use this metric to differentiate the top 5 performing dealerships. Note that using a weighed average may be more appropriate for finding profit per employee.  However, the margins per sales type are not provided*  Also, since I want to get a good idea of profit, I will exclude sales which were returned*/
  
 /*Unique employees per dealership range from 74 to 115.  This represents a range of 41 employees across all 50 dealerhsips.*/ 
 
-/*Here I will use 'dms' which represents "dealership mean sales" revenue per employee*/
+-- Sales per Employee CTE
 WITH dms AS 
 (
-SELECT
-	s.dealership_id,
-	-- avg_spe = average sales per employee
-	'$' ||''|| ROUND(SUM(s.price) / COUNT(DISTINCT s.employee_id), 2) AS avg_spe
-FROM
-	sales s
-GROUP BY
-	s.dealership_id
-ORDER BY
-	avg_spe DESC
-LIMIT 5;
+	SELECT
+		s.dealership_id,
+		d.business_name,
+		-- avg_spe = sales per employee where the vehicle was not returned
+		'$' ||''|| ROUND(SUM(s.price) / COUNT(DISTINCT s.employee_id), 2) AS avg_spe
+	FROM sales s
+	LEFT JOIN dealerships AS d ON s.dealership_id = d.dealership_id
+	--I am going to exclude vehicles that were returned from revenue generated
+	WHERE s.sale_returned = FALSE
+	GROUP BY s.dealership_id, d.business_name
 ),
-WITH average AS
--- Average revenue per dealership.  Again, 50 dealerships are represented and have unique values for each
+-- Ranked non-returned sales per employee per dealership CTE
+d_rank AS
 (
-SELECT
-	s.dealership_id,
-	-- average of revenue per dealership
-	SUM(s.price) AS revenue
-FROM
-	sales s
-GROUP BY
-	s.dealership_id
-ORDER BY
-	dealership_id
+	SELECT
+		s.dealership_id,
+		s.employee_id,
+		e.first_name ||' '|| e.last_name AS top_seller,
+		COUNT(s.sale_id) AS transactions,
+		ROW_NUMBER() OVER(PARTITION BY s.dealership_id ORDER BY COUNT(s.sale_id) DESC) AS rank
+	FROM sales s
+	LEFT JOIN employees e ON s.employee_id = e.employee_id
+	WHERE s.sale_returned = FALSE
+	GROUP BY s.dealership_id, s.employee_id, e.first_name, e.last_name
+),
+-- Most popular models which were not returned
+top_model AS
+(
+	SELECT
+		s.dealership_id,
+		vt.model,
+		COUNT(vt.model) as model_sales,
+		ROW_NUMBER() OVER(PARTITION BY s.dealership_id ORDER BY COUNT(s.sale_id) DESC) AS rank
+	FROM sales s
+	LEFT JOIN vehicles as v ON s.vehicle_id = v.vehicle_id
+	LEFT JOIN vehicletypes AS vt ON v.vehicle_type_id = vt.vehicle_type_id
+	WHERE s.sale_returned = FALSE
+	GROUP BY s.dealership_id, vt.model
+),
+-- Purchases vs. Leases majority per dealership
+order_type AS
+(
+	SELECT
+		s.dealership_id,
+		st.sales_type_name AS transaction,
+		COUNT(s.sales_type_id) AS count,
+	-- There is only one "rent" record, all others are purchases or leases
+		ROW_NUMBER() OVER(PARTITION BY s.dealership_id ORDER BY COUNT(s.sales_type_id) DESC) AS rank
+	FROM sales AS s
+	LEFT JOIN salestypes st ON s.sales_type_id = st.sales_type_id
+	WHERE s.sales_type_id IN (1,2)
+	GROUP BY s.dealership_id, st.sales_type_name
 )
+-- Main Query
 SELECT
-	weight.employees + average.revenue / weight.employees as weighted_average
-
-/*For the top 5 dealerships, which vehicle models were the most popular in sales?*/
-select* FROM sales
-
-/*For the top 5 dealerships, were there more sales or leases?*/
-
+	d.business_name AS "Top 5 Dealerships",
+	d.avg_spe AS "Per-Employee Transaction Mean",
+	r.top_seller AS "Top Performing Employee by Sales Volume",
+	tm.model AS "Most Popular Car Model",
+	ot.transaction AS "Top Transaction Type"
+FROM dms d
+LEFT JOIN d_rank AS r ON d.dealership_id = r.dealership_id AND r.rank = 1
+LEFT JOIN top_model AS tm ON d.dealership_id = tm.dealership_id AND tm.rank = 1
+LEFT JOIN order_type AS ot ON d.dealership_id = ot.dealership_id and ot.rank = 1
+ORDER BY d.avg_spe DESC
+LIMIT 5;
