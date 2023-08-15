85cc66fd389ff63a50f2274f6e4c455c2a441fae
Claytor <tim@claytor.me>
2023-08-15 15:38:27 -0500

    Did a brief analysis of sales.  Found out some cool formatting options.

---
+++ Book 2 - Carnival Reports/ch9_carnival_sales.sql
@@ -0,0 +1,70 @@
+-- ========================================
+-- Purchase Income by Dealership
+-- ========================================
+
+-- Write a query that shows the total purchase sales income per dealership.
+SELECT 
+	d.business_name AS "Dealership",
+	'$' || TO_CHAR(SUM(s.price), 'FM99,999,999.00') AS "Total Sales Income"
+FROM sales s
+LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+WHERE sales_type_id = 1
+GROUP BY d.business_name;
+
+-- Write a query that shows the purchase sales income per dealership for all of 2020.
+SELECT 
+	d.business_name AS "Dealership",
+	'$' || TO_CHAR(SUM(s.price), 'FM99,999,999.00') AS "2020 Purchase Sales Income"
+FROM sales s
+LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+WHERE sales_type_id = 1 
+	AND EXTRACT(YEAR FROM purchase_date) = 2020
+GROUP BY d.business_name;
+
+-- ========================================
+-- Lease Income by Dealership
+-- ========================================
+
+-- Write a query that shows the total lease income per dealership.
+SELECT 
+	d.business_name AS "Dealership",
+	'$' || TO_CHAR(SUM(s.price), 'FM9,999,999.00') AS "Total Lease Income"
+FROM sales s
+LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+WHERE sales_type_id = 2 
+GROUP BY d.business_name;
+
+-- Write a query that shows the lease income per dealership for Jan of 2020.
+SELECT 
+	d.business_name AS "Dealership",
+	'$' || TO_CHAR(SUM(s.price), 'FM999,999.00') AS "January 2020 Lease Income"
+FROM sales s
+LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+WHERE sales_type_id = 2 
+	AND EXTRACT(YEAR FROM purchase_date) = 2020
+	AND EXTRACT(MONTH FROM purchase_date) = 1
+GROUP BY d.business_name;
+
+-- Write a query that shows the lease income per dealership for all of 2019.
+SELECT 
+	d.business_name AS "Dealership",
+	'$' || TO_CHAR(SUM(s.price), 'FM999,999.00') AS "2019 Lease Income"
+FROM sales s
+LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+WHERE sales_type_id = 2 
+	AND EXTRACT(YEAR FROM purchase_date) = 2019
+GROUP BY d.business_name;
+
+-- ========================================
+-- Total Income by Employee
+-- ========================================
+
+-- Write a query that shows the total income (purchase and lease) per employee.
+SELECT 
+	e.first_name ||' '|| e.last_name AS "Employee",
+	'$' || TO_CHAR(SUM(s.price), 'FM999,999.00') AS "Total Combined Sales & Lease Income"
+FROM sales s
+LEFT JOIN employees e ON s.employee_id = e.employee_id
+WHERE sales_type_id IN (1,2) 
+GROUP BY e.last_name, e.first_name
+ORDER BY e.last_name;
