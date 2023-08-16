ca27879be6cddf67df46ae445f173f7816d8384e
Claytor <tim@claytor.me>
2023-08-15 21:52:07 -0500

    Updated some typos

---
+++ Book 2 - Carnival Reports/ch8_window_funcitons_(self_generated).sql
@@ -0,0 +1,56 @@
+/* 
+1. Employee Performance Analysis
+    - Which employee has the highest sales in terms of total sales price for each dealership?
+    - For each employee, how do their sales rank within their respective dealership based on the total sales price?
+*/
+
+/* 
+2. Customer Analysis
+    - Identify customers who have made multiple purchases. Rank them based on their total purchase amount.
+    - Which customer made the earliest purchase for each dealership?
+*/
+
+/* 
+3. Vehicle Sales Trend Analysis
+    - For each vehicle model, calculate the running total of sales across time.
+    - For each year, find out which vehicle model was the most popular in terms of sales volume.
+*/
+
+/* 
+4. Service and Repair Analysis
+    - How many vehicles are repeatedly coming for the same type of repair? Identify patterns by grouping them by repair type.
+    - Calculate the average time between oil changes for each vehicle and rank the vehicles based on this average time.
+*/
+
+/* 
+5. Inventory Turnover Analysis
+    - For each dealership, determine the average time taken to sell a new vehicle after it has been added to inventory. 
+    - Identify vehicles that have been in inventory for longer than the average time per dealership.
+*/
+
+/* 
+6. Price and Sale Analysis
+    - Compare each sale's price to the average sale price of the respective dealership. Identify which sales were above the average and which were below.
+    - For each vehicle model, calculate the percentage difference between the floor_price and the actual sale price.
+*/
+
+/* 
+7. Employee Type Analysis
+    - Rank employee types based on the average sales they make in all dealerships combined.
+    - For each type of employee, find the employee who has the maximum sales in terms of volume and price.
+*/
+
+/* 
+8. Customer Return Analysis
+    - Identify customers who have returned their purchased vehicles. For each of these customers, rank their returns based on the sale price.
+*/
+
+/* 
+9. Vehicle Maintenance Analysis
+    - Identify vehicles that have the highest frequency of oil changes and repairs. Rank them to provide insights into potential issues or overuse.
+*/
+
+/* 
+10. Sales Type Analysis
+    - For each sales type (like purchase, lease, etc.), calculate the running total and the percentage of total sales they represent over time.
+*/
