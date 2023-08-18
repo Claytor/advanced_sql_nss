8734cb529848a61d1492aaa511488dc2eba99f3f
Claytor <tim@claytor.me>
2023-08-17 20:51:08 -0500

    Created views for useful queries.

--- Book 2 - Carnival Reports/ch9_carnival_sales.sql
+++ Book 2 - Carnival Reports/ch9_carnival_sales.sql
@@ -3,13 +3,15 @@
 -- ========================================
 
 -- Write a query that shows the total purchase sales income per dealership.
-SELECT 
-	d.business_name AS "Dealership",
-	'$' || TO_CHAR(SUM(s.price), 'FM99,999,999.00') AS "Total Sales Income"
-FROM sales s
-LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
-WHERE sales_type_id = 1
-GROUP BY d.business_name;
+-- This will be useful later, so I'll make it a view
+CREATE VIEW total_purchase_income_per_dealership AS
+	SELECT 
+		d.business_name AS "Dealership",
+		'$' || TO_CHAR(SUM(s.price), 'FM99,999,999.00') AS "Total Sales Income"
+	FROM sales s
+	LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+	WHERE sales_type_id = 1
+	GROUP BY d.business_name;
 
 -- Write a query that shows the purchase sales income per dealership for all of 2020.
 SELECT 
@@ -26,13 +28,15 @@ GROUP BY d.business_name;
 -- ========================================
 
 -- Write a query that shows the total lease income per dealership.
-SELECT 
-	d.business_name AS "Dealership",
-	'$' || TO_CHAR(SUM(s.price), 'FM9,999,999.00') AS "Total Lease Income"
-FROM sales s
-LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
-WHERE sales_type_id = 2 
-GROUP BY d.business_name;
+-- This will be useful later, so I'll make it a view
+CREATE VIEW total_lease_income_per_dealership AS
+	SELECT 
+		d.business_name AS "Dealership",
+		'$' || TO_CHAR(SUM(s.price), 'FM9,999,999.00') AS "Total Lease Income"
+	FROM sales s
+	LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id
+	WHERE sales_type_id = 2 
+	GROUP BY d.business_name;
 
 -- Write a query that shows the lease income per dealership for Jan of 2020.
 SELECT 
@@ -60,11 +64,13 @@ GROUP BY d.business_name;
 -- ========================================
 
 -- Write a query that shows the total income (purchase and lease) per employee.
-SELECT 
-	e.first_name ||' '|| e.last_name AS "Employee",
-	'$' || TO_CHAR(SUM(s.price), 'FM999,999.00') AS "Total Combined Sales & Lease Income"
-FROM sales s
-LEFT JOIN employees e ON s.employee_id = e.employee_id
-WHERE sales_type_id IN (1,2) 
-GROUP BY e.last_name, e.first_name
-ORDER BY e.last_name;
+-- This will be useful later, so I'll make it a view
+CREATE VIEW total_combined_income_per_employee AS
+	SELECT 
+		e.first_name ||' '|| e.last_name AS "Employee",
+		'$' || TO_CHAR(SUM(s.price), 'FM999,999.00') AS "Total Combined Sales & Lease Income"
+	FROM sales s
+	LEFT JOIN employees e ON s.employee_id = e.employee_id
+	WHERE sales_type_id IN (1,2) 
+	GROUP BY e.last_name, e.first_name
+	ORDER BY e.last_name;
