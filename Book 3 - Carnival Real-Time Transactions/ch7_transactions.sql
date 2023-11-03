db9a972612190a00dd028eb8aad4d96140bfa784
Claytor <tim@claytor.me>
2023-11-03 14:12:56 -0500

    ch_readme

---
+++ Book 3 - Carnival Real-Time Transactions/ch7_transactions.sql
@@ -0,0 +1,41 @@
+-- ========================================
+-- PRACTICE PROBLEM 1: Add Automotive Mechanic Role and Mechanics
+-- ========================================
+-- Write a transaction to:
+-- 1. Add a new role for employees named 'Automotive Mechanic'.
+-- 2. Add five new mechanics (you can choose the data).
+-- 3. Ensure each of the new mechanics works at the specified three dealerships.
+/*
+BEGIN;
+
+-- Your code here
+
+COMMIT;
+*/
+
+-- ========================================
+-- PRACTICE PROBLEM 2: New Dealership and Employees
+-- ========================================
+-- Create a transaction to:
+-- 1. Add a new dealership in Washington, D.C. named 'Felphun Automotive'.
+-- 2. Hire three new employees for this dealership: Sales Manager, General Manager, and Customer Service.
+/*
+BEGIN;
+
+-- Your code here
+
+COMMIT;
+*/
+
+-- ========================================
+-- PRACTICE PROBLEM 3: Shift Employees to a New Dealership
+-- ========================================
+-- Write a transaction to:
+-- Move all employees from 'Nelsen Autos of Illinois' to 'Cain Autos of Missouri'.
+/*
+BEGIN;
+
+-- Your code here
+
+COMMIT;
+*/
