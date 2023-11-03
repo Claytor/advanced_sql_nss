20aa37157b229174c3b8faa3baff543bd01f32f1
Claytor <tim@claytor.me>
2023-11-03 14:12:34 -0500

    ch_readme

---
+++ Book 3 - Carnival Real-Time Transactions/ch8_carnival_vehicle_transactions.sql
@@ -0,0 +1,17 @@
+-- -----------------------------------------------------------------------------
+-- Vehicle Transactions
+-- -----------------------------------------------------------------------------
+
+-- Add 5 brand new 2021 Honda CR-Vs to the inventory. They have I4 engines and are classified as a Crossover SUV or CUV. 
+-- All of them have beige interiors but the exterior colors are Lilac, Dark Red, Lime, Navy and Sand. 
+-- The floor price is $21,755 and the MSR price is $18,999.
+-- [Your code here]
+
+-- For the CX-5s and CX-9s in the inventory that have not been sold, change the year of the car to 2021 since we will be updating our stock of Mazdas. 
+-- For all other unsold Mazdas, update the year to 2020. The newer Mazdas all have red and black interiors.
+-- [Your code here]
+
+-- The vehicle with VIN KNDPB3A20D7558809 is about to be returned. Carnival has a pretty cool program where it offers the returned vehicle to 
+-- the most recently hired employee at 70% of the cost it previously sold for. The most recent employee accepts this offer and will purchase the vehicle 
+-- once it is returned. The employee and dealership who sold the car originally will be on the new sales transaction.
+-- [Your code here]
